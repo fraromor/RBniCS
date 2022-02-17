f55217e3ae08cc313e979763c6e98dc0c4f53c2a
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-02-17 18:47:21 +0100

    Use MPI_COMM_WORLD by default when reading in a functions list of online functions from file

--- rbnics/backends/online/numpy/wrapping/get_mpi_comm.py
+++ rbnics/backends/online/numpy/wrapping/get_mpi_comm.py
@@ -4,6 +4,12 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
+from mpi4py.MPI import COMM_WORLD
 
-def get_mpi_comm(basis_functions):
-    return basis_functions.mpi_comm
+
+def get_mpi_comm(arg):
+    if isinstance(arg, int):
+        return COMM_WORLD
+    else:
+        basis_functions = arg
+        return basis_functions.mpi_comm

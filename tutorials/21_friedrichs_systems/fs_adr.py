aebce7039acc1d191b4742c24505636c05131823
fromor <francesco.romor@gmail.com>
2022-03-16 17:30:42 +0100

    fixed fs adr bugs

--- tutorials/21_friedrichs_systems/fs_adr.py
+++ tutorials/21_friedrichs_systems/fs_adr.py
@@ -5,8 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import logging
+
 logging.basicConfig(filename='adr.log', level=logging.DEBUG)
 logging.info('Started')
+
 
 class BoundaryM(UserExpression):
     def __init__(self, normal, **kwargs):
@@ -18,7 +20,7 @@ class BoundaryM(UserExpression):
             or (abs(x[1]) < DOLFIN_EPS or abs(x[1] - 1.0) < DOLFIN_EPS):
             values[2] = -self.normal(x)[0]
             values[5] = -self.normal(x)[1]
-            values[8] = 0 # Dirichlet
+            values[8] = 0  # Dirichlet
             #values[8] = inner(self.beta, self.normal)(x) # Neumann
             values[6] = self.normal(x)[0]
             values[7] = self.normal(x)[1]
@@ -29,9 +31,9 @@ class BoundaryM(UserExpression):
         else:
             values = [0] * 9
 
-
     def value_shape(self):
         return ((3, 3))
+
 
 class AdvectionDiffusionReaction(FriedrichsSystemProblem):
     def __init__(self, L, **kwargs):
@@ -51,14 +53,14 @@ class AdvectionDiffusionReaction(FriedrichsSystemProblem):
         self.dx = Measure("dx")(domain=self.mesh,
                                 subdomain_data=self.subdomains)
         self.dS = Measure("dS")(domain=self.mesh,
-                                subdomain_data=self.boundaries)
+                                subdomain_data=self.subdomains)
         self.ds = Measure("ds")(domain=self.mesh,
                                 subdomain_data=self.boundaries)
 
         self.n = FacetNormal(self.mesh)
         boundaryMesh = BoundaryMesh(mesh, 'exterior')
         self.normalCustom = self.get_facet_normal(boundaryMesh)
-        self.alpha = Constant(1)
+        self.alpha = Constant(10.)
 
         self.A_0_kappa = Expression(
             (('1', '0', '0'), ('0', '1', '0'), ('0', '0', '0')),
@@ -81,11 +83,11 @@ class AdvectionDiffusionReaction(FriedrichsSystemProblem):
             (("0", "0", "0"), ("0", "0", "0"), ("0", "0", "1")),
             element=X.ufl_element())
 
-        self.D = (self.A_1_0 + self.A_1_1) * self.n[0] + (
-            self.A_2_0 + self.A_2_1) * self.n[1]
+        self.D_0 = self.A_1_0 * self.n[0] + self.A_2_0 * self.n[1]
+        self.D_1 = self.A_1_1 * self.n[0]
+        self.D_2 = self.A_2_1 * self.n[1]
 
-        self.M = BoundaryM(normal=self.normalCustom,
-                      element=X.ufl_element())
+        self.M = BoundaryM(normal=self.normalCustom, element=X.ufl_element())
 
         self.f = Expression(
             ("0.0", "0.0", "10*exp(-pow((x[0]-0.5), 2)-pow((x[1]-0.5), 2))"),
@@ -106,15 +108,16 @@ class AdvectionDiffusionReaction(FriedrichsSystemProblem):
         normals = np.ones(vec.shape)
         normals[:, 0] = vec[:, 1]
         normals[:, 1] = -vec[:, 0]
-        origins = (vertices[cells[:, 1]] + vertices[cells[:, 0]])/2
+        origins = (vertices[cells[:, 1]] + vertices[cells[:, 0]]) / 2
         normals /= np.sqrt((normals**2).sum(axis=1))[:, np.newaxis]
         # plt.quiver(*origins.T, normals[:, 0], normals[:, 1])
         # plt.show()
         # plt.quiver(*origins.T, normals[:, 0], normals[:, 1])
 
         # Ensure outward pointing normal
-        bmesh.init_cell_orientations(Expression(('x[0]-0.5', 'x[1]-0.5'), degree=1))
-        mask = np.array(bmesh.cell_orientations())== 0
+        bmesh.init_cell_orientations(
+            Expression(('x[0]-0.5', 'x[1]-0.5'), degree=1))
+        mask = np.array(bmesh.cell_orientations()) == 0
         normals[mask, :] = -1 * normals[mask, :]
         # plt.quiver(*origins.T, normals[:, 0], normals[:, 1])
         # plt.scatter(origins[:, 0], origins[:, 1], c=bmesh.cell_orientations())
@@ -124,7 +127,7 @@ class AdvectionDiffusionReaction(FriedrichsSystemProblem):
         norm = Function(V)
         nv = norm.vector()
 
-        for n in (0,1):
+        for n in (0, 1):
             dofmap = V.sub(n).dofmap()
             for i in np.arange(dofmap.global_dimension()):
                 dof_indices = dofmap.cell_dofs(i)
@@ -136,16 +139,22 @@ class AdvectionDiffusionReaction(FriedrichsSystemProblem):
     def compute_theta(self, term):
         mu = self.mu
         if term == "a":
-            theta_a0kappa = mu[0]#1/kappa
-            theta_a0nu = mu[1]#nu
+            theta_a0kappa = 1/mu[0]  #kappa=viscosity
+            theta_a0nu = mu[1]  #nu
             theta_a10 = 1.
-            theta_a11 = mu[2]#beta1
+            theta_a11 = mu[2]  #beta1
             theta_a20 = 1.
-            theta_a21 = mu[3]#beta2
-            theta_D = 1.
+            theta_a21 = mu[3]  #beta2
+            theta_D_0 = 1.
+            theta_D_1 = mu[2]  #beta1
+            theta_D_2 = mu[3]  #beta2
             theta_S = 1.
-            theta_DpM = 1.
-            return (theta_a0kappa, theta_a0nu, theta_a10, theta_a11, theta_a20, theta_a21, theta_D, theta_S, theta_DpM)
+            theta_DpM_0 = 1.
+            theta_DpM_1 = mu[2]  #beta1
+            theta_DpM_2 = mu[3]  #beta2
+            return (theta_a0kappa, theta_a0nu, theta_a10, theta_a11, theta_a20,
+                    theta_a21, theta_D_0, theta_D_1, theta_D_2, theta_S,
+                    theta_DpM_0, theta_DpM_1, theta_DpM_2)
         elif term == "f":
             theta_f0 = 1.
             return (theta_f0, )
@@ -163,17 +172,22 @@ class AdvectionDiffusionReaction(FriedrichsSystemProblem):
             a_a11 = -inner(z, dot(self.A_1_1, y.dx(0))) * dx
             a_a20 = -inner(z, dot(self.A_2_0, y.dx(1))) * dx
             a_a21 = -inner(z, dot(self.A_2_1, y.dx(1))) * dx
-            aD = inner(dot(self.D('+'), avg(z)), jump(y)) * dS
+            aD_0 = inner(dot(self.D_0('+'), avg(z)), jump(y)) * dS
+            aD_1 = inner(dot(self.D_1('+'), avg(z)), jump(y)) * dS
+            aD_2 = inner(dot(self.D_2('+'), avg(z)), jump(y)) * dS
             aS = self.alpha('+') * inner(jump(z), jump(y)) * dS
-            aDpM = 0.5 * inner(dot(self.D + self.M, z), y) * ds
-            return (a_a0kappa, a_a0nu, a_a10, a_a11, a_a20, a_a21, aD, aS, aDpM)
+            aDpM_0 = 0.5 * inner(dot(self.D_0 + self.M, z), y) * ds
+            aDpM_1 = 0.5 * inner(dot(self.D_1, z), y) * ds
+            aDpM_2 = 0.5 * inner(dot(self.D_2, z), y) * ds
+            return (a_a0kappa, a_a0nu, a_a10, a_a11, a_a20, a_a21, aD_0, aD_1,
+                    aD_2, aS, aDpM_0, aDpM_1, aDpM_2)
         elif term == "f":
             f = self.f
             f0 = dot(y, f) * dx
             return (f0, )
         elif term == "inner_product":
             x0 = inner(grad(z), grad(y)) * dx
-            return (x0,)
+            return (x0, )
         else:
             raise ValueError("Invalid term for assemble_operator().")
 
@@ -187,8 +201,11 @@ boundaries = MeshFunction("size_t", mesh, "data/square_facet_region.xml")
 L = VectorFunctionSpace(mesh, "DG", 1, dim=3)
 
 # 3. Allocate an object of the Friedrichs' systems class
-problem = AdvectionDiffusionReaction(L, mesh= mesh, subdomains=subdomains, boundaries=boundaries)
-mu_range = [(1.0, 1e2), (1e-4, 1.0), (0, 1), (0, 1)]
+problem = AdvectionDiffusionReaction(L,
+                                     mesh=mesh,
+                                     subdomains=subdomains,
+                                     boundaries=boundaries)
+mu_range = [(0.1, 1), (1, 1), (-1, 1), (1, 1)]
 problem.set_mu_range(mu_range)
 
 # 4. Prepare reduction with a POD-Galerkin method
@@ -200,13 +217,23 @@ reduction_method.initialize_training_set(100, sampling=UniformDistribution())
 reduced_problem = reduction_method.offline()
 
 # 6. Perform an online solve
-online_mu = (1, 1, 0.5, 0.5)
+online_mu = (0.1, 1, 1, 1)
 reduced_problem.set_mu(online_mu)
+reduction_method.truth_problem.set_mu(online_mu)
+
+# Plot solution
+snapshot = reduction_method.truth_problem.solve()
+u_sol = snapshot.sub(2)
+p = plot(u_sol, interactive=True)
+plt.colorbar(p)
+plt.show()
+
 reduced_problem.solve()
 reduced_problem.export_solution(filename="online_solution")
 
 # 7. Perform an error analysis
 reduction_method.initialize_testing_set(100, sampling=UniformDistribution())
+
 reduction_method.error_analysis()
 
 # 8. Perform a speedup analysis
